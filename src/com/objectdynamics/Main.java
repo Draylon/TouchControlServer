aa45a45ac757b473ed92468980e09f4d711da3b0
Draylon <draylid@gmail.com>
2020-08-01 20:18:19 -0300

    fixed lag fixed comms mechanism abandoning udp comms for now fixed compression length mechanism

--- src/com/objectdynamics/Main.java
+++ src/com/objectdynamics/Main.java
@@ -1,17 +1,15 @@
 package com.objectdynamics;
 
+import com.sun.media.sound.RealTimeSequencerProvider;
+import sun.misc.BASE64Encoder;
+
 import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageOutputStream;
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.InputEvent;
-import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBuffer;
-import java.awt.image.DataBufferByte;
 import java.io.*;
-import java.net.Socket;
-import java.util.Scanner;
+import java.net.*;
+import java.util.zip.Deflater;
 
 enum MouseFunctionType{
     TRACKPAD,
@@ -29,27 +27,6 @@ enum MouseAutomaton{
     RIGHT_CLICK,
     SCROLL_DOWN,
     SCROLL_UP
-}
-
-class TranslucentWindowMin extends JFrame {
-    public TranslucentWindowMin() {
-        setTitle("Translucent Window Example");
-        setSize(300,300);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridBagLayout());
-        // Set the window to 45% opaque = 55% translucent
-        setOpacity(0.45f);
-    }
-    public void displayWindow(){
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new TranslucentWindowMin().setVisible(true);
-            }
-        });
-    }
 }
 
 class WindowsDrawingFrame{
@@ -82,10 +59,11 @@ class WindowsDrawingFrame{
         this.yScaleFactor=tabletHeight/(this.height-this.y);
         this.captureRegion = new Rectangle(this.x,this.y,this.width,this.height);
     }
-    public DataBuffer getScreen() throws IOException {
-        capturedImage = rob.createScreenCapture(this.captureRegion);
-        //byte[] imageBytes = ((DataBufferByte) bufferedImage.getData().getDataBuffer()).getData();
-        return capturedImage.getRaster().getDataBuffer();
+    String imageString=null;
+    ByteArrayOutputStream bos;
+    BASE64Encoder encoder = new BASE64Encoder();
+    public BufferedImage getBufferedImage() throws IOException {
+        return rob.createScreenCapture(this.captureRegion);
     }
 }
 
@@ -252,8 +230,8 @@ class MouseController{
 }
 
 public class Main {
-    private void displayScreen(){
-        /*Window w=new Window(null)
+    /*private void displayScreen(){
+        Window w=new Window(null)
         {
             @Override
             public void paint(Graphics g)
@@ -276,8 +254,8 @@ public class Main {
         w.setAlwaysOnTop(true);
         w.setBounds(w.getGraphicsConfiguration().getBounds());
         w.setBackground(new Color(0, true));
-        w.setVisible(true);*/
-    }
+        w.setVisible(true);
+    }*/
     static MouseController mouseController;
     static WindowsDrawingFrame windowsDrawingFrame;
     static Robot rob;
@@ -286,60 +264,178 @@ public class Main {
     private static String clipString="";
     private static int tempID=-1;
     private static boolean workData=false;
+    static int iip=0;
+    //https://stackoverflow.com/questions/6116880/stream-live-video-from-phone-to-phone-using-socket-fd
+    //https://stackoverflow.com/questions/6973848/sending-a-screenshot-bufferedimage-over-a-socket-in-java
 
     private static void bindScreenServer(){
-        Thread t = new Thread(new Runnable() {
-            Socket client;
-            DataOutputStream dOs;
-            @Override
-            public void run() {
-                try {
-                    client = new Socket("127.0.0.1",5546);
-                    if(client==null){ System.out.println("Screen server error");return; }
-                    dOs = new DataOutputStream(client.getOutputStream());
-                    while (client.isConnected()){
-                        dOs.writeBytes(windowsDrawingFrame.getScreen().toString());
-                        dOs.flush();
-                        Thread.sleep(66);
+        Thread t = new Thread(() -> {
+            Socket client=null;
+            DataOutputStream dOs=null;
+            ByteArrayOutputStream bOsg;
+            try {
+                client = new Socket("127.0.0.1",5546);
+                if(client==null||!client.isConnected()||client.isClosed() || !client.isBound()){System.out.println("Screen server error connecting");return;}
+                System.out.println("screen Connected! "+client.isConnected()+"\n"+client.toString());
+                dOs = new DataOutputStream(client.getOutputStream());
+                compressor= new Deflater();
+                compressor.setLevel(Deflater.BEST_COMPRESSION);
+                encscrDOS=new ByteArrayOutputStream();
+                bOsg=new ByteArrayOutputStream();
+                while (client.isConnected()){
+                    ImageIO.write(windowsDrawingFrame.getBufferedImage(),"jpg",encscrDOS);
+                    System.out.println("fetching uncompressed "+encscrDOS.size());
+                    //complen = sourceLen - ((sourceLen + 7) >> 3) - ((sourceLen + 63) >> 6) + 5;
+                    //int notation = (encscrDOS.size() - ((encscrDOS.size() + 7) >> 3) - ((encscrDOS.size() + 63) >> 6) + 5);
+                    //System.out.println("compression result: "+notation);
+                    try{encscrDOS.close();}catch (Exception e){}
+                    compressor.setInput(encscrDOS.toByteArray());
+                    compressor.finish();
+                    byte[] buf = new byte[8192];
+                    while (!compressor.finished()) {
+                        int count = compressor.deflate(buf);
+                        bOsg.write(buf, 0, count);
                     }
-                    dOs.close();
-                    client.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    System.out.println("sending compressed: "+bOsg.size());
+                    dOs.writeInt(bOsg.size());
+                    dOs.write(bOsg.toByteArray());
+                    dOs.flush();
+                    System.out.println("Sent");
+                    encscrDOS.flush();
+                    bOsg.flush();
+                    bOsg.reset();
+                    encscrDOS.reset();
+                    compressor.reset();
+                    Thread.sleep(40);
                 }
+                dOs.close();
+                client.close();
+            }catch (java.net.SocketException e){
+                System.err.println("Data Output (ScreenCast) not avaliable / properly connected");
+                e.printStackTrace();
+                try { if(dOs!=null)client.close(); } catch (IOException ex) { ex.printStackTrace(); }
+                try { if(client!=null)client.close(); } catch (IOException ex) { ex.printStackTrace(); }
+            }catch (Exception e) {
+                System.err.println("screenBind Erro: "+e.getMessage());
             }
         });
         t.start();
     }
-    private static void bindTouchServer(){
-        Thread t = new Thread(new Runnable() {
+
+    private static DatagramSocket screenClient=null;
+    private static ByteArrayOutputStream scrDOS=null;
+    private static ByteArrayOutputStream encscrDOS=null;
+    private static Deflater compressor=null;
+    private static InetAddress ipaddr;
+
+    /*private static void bindScreenServer(){
+        Thread sendVideo = new Thread(new Runnable() {
             @Override
             public void run() {
-                Socket client;
                 try{
-                    client = new Socket("127.0.0.1",5545);
-                    if(client==null) {
-                        System.out.println("error connecting, closing now");
-                        return;
-                    }
+                    // Compressor with highest level of compression
+                    compressor= new Deflater();
+                    compressor.setLevel(Deflater.BEST_COMPRESSION);
+                    screenClient = new DatagramSocket(5546);
+                    scrDOS=new ByteArrayOutputStream();
+                    encscrDOS=new ByteArrayOutputStream();
+                    ipaddr = InetAddress.getByName("127.0.0.1");
 
-                    System.out.println("Connected! "+client.isConnected()+"\n"+client.toString());
-                    //Scanner ston = new Scanner(client.getInputStream());
-                    DataInputStream ston = new DataInputStream(client.getInputStream());
-                    while(client.isConnected()){
-                        //System.out.println("Stonks "+ (char)ston.readByte() );
-                        setClipString(ston.readByte());
-                        //mouseController.proccessData(ston.readUTF());
-                    }
-                    if(client != null)
-                        try{
-                            client.close();
-                        }catch (Exception ex){
-                            System.err.println(ex);
+                    while(screenClient.isBound()&&!screenClient.isClosed()){
+                        ImageIO.write(windowsDrawingFrame.getBufferedImage(),"jpg",encscrDOS);
+                        System.out.println("fetching "+encscrDOS.size());
+                        try{encscrDOS.close();}catch (Exception e){}
+                        compressor.setInput(encscrDOS.toByteArray());
+                        compressor.finish();
+                        byte[] buf = new byte[2048];
+                        while (!compressor.finished()) {
+                            int count = compressor.deflate(buf);
+                            scrDOS.write(buf, 0, count);
                         }
+                        try { scrDOS.close(); } catch (IOException e) { }
+                        byte[] compressedData = scrDOS.toByteArray();
+                        System.out.println("sending compressed "+scrDOS.size());
+                        DatagramPacket dgp=new DatagramPacket(compressedData,0,compressedData.length,ipaddr,5546);
+                        screenClient.send(dgp);
+                        scrDOS.reset();
+                        encscrDOS.reset();
+                        compressor.reset();
+                        System.out.println("Sent package");
+                        Thread.sleep(10000);
+                    }
                 }catch (Exception e){
+                    System.out.println(e.getMessage());
                     e.printStackTrace();
                 }
+            }
+        });
+        sendVideo.start();
+    }*/
+
+    /*Runnable sendScreenServerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            // From Server.java
+            try {
+                screenServer = new ServerSocket(3323);
+                while (true) {
+                    //final ParcelFileDescriptor pfd = ParcelFileDescriptor.fromSocket(client);
+                    recorder = new MediaRecorder();
+                    recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                    recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                    recorder.setOutputFile(pfd.getFileDescriptor());
+                    recorder.setVideoFrameRate(20);
+                    recorder.setVideoSize(176, 144);
+                    recorder.setVideoEncoder(MediaRecorder.VideoEncoder.H263);
+                    recorder.setPreviewDisplay(mHolder.getSurface());
+                    try {
+                        recorder.prepare();
+                    } catch (IllegalStateException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    recorder.start();
+                }
+            } catch (UnknownHostException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    };*/
+
+
+    private static void bindTouchServer(){
+        Thread t = new Thread(() -> {
+            Socket client=null;
+            DataInputStream dIs=null;
+            try{
+                client = new Socket("127.0.0.1",5545);
+                if(client==null||!client.isConnected()||client.isClosed() || !client.isBound()) { System.out.println("touch server error connecting, closing now");return; }
+                System.out.println("Touch Connected! "+client.isConnected()+"\n"+client.toString());
+                dIs=new DataInputStream(client.getInputStream());
+                while(client.isConnected()){
+                    setClipString(dIs.readByte());
+                }
+                if(client != null)
+                    try{
+                        client.close();
+                    }catch (Exception ex){
+                        System.err.println(ex);
+                    }
+            }catch (EOFException e){
+                System.err.println("Data Input (Touch) not avaliable / properly connected");
+                e.printStackTrace();
+                try { if(dIs!=null)client.close();dIs=null;} catch (IOException ex) { ex.printStackTrace(); }
+                try { if(client!=null)client.close();client=null; } catch (IOException ex) { ex.printStackTrace(); }
+            } catch (Exception e){
+                System.err.println("touchBind Erro: "+e.getMessage());
+                e.printStackTrace();
             }
         });
         t.start();
@@ -393,14 +489,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         try  {
             rob= new Robot();
-            windowsDrawingFrame=new WindowsDrawingFrame(rob,0,0,640,480);
+            windowsDrawingFrame=new WindowsDrawingFrame(rob,0,0,800,480);
             mouseController = new MouseController(windowsDrawingFrame,rob);
             System.out.println("CONNECTING...");
-            bindTouchServer();
+            //bindTouchServer();
             bindScreenServer();
         }catch (Exception e) {
             System.out.println("Erro!: "+e.getMessage());
         }
-
     }
 }
